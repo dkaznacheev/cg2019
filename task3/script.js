69e158d1b93404af63afeec29b0c83ffb82fd171
dkaznacheev <dmitry.kaznacheev@gmail.com>
2019-12-24 00:21:03 +0300

    Voxelizing faces almost coplanar to secting plane

--- task3/script.js
+++ task3/script.js
@@ -22,12 +22,18 @@ function addLight() {
     scene.add( hlight );
 }
 
+function intersect_section(v1, v2, z) {
+    return new THREE.Vector3(v1.x + (z - v1.z) / (v2.z - v1.z) * (v2.x - v1.x),
+                             v1.y + (z - v1.z) / (v2.z - v1.z) * (v2.y - v1.y),
+                             z);
+}
+
 function intersect_face(v1, v2, v3, z, blockSize) {
     if (v1.z >= z) {
         return [];
     }
     
-    if (v1.z > z - blockSize && v3.z < z) {
+    if (v1.z > z - 2 * blockSize && v1.z < z && v3.z < z) {
         //project plane
         return [[new THREE.Vector3(v1.x, v1.y, z), new THREE.Vector3(v2.x, v2.y, z)],
                 [new THREE.Vector3(v2.x, v2.y, z), new THREE.Vector3(v3.x, v3.y, z)],
@@ -37,9 +43,11 @@ function intersect_face(v1, v2, v3, z, blockSize) {
     if (v3.z < z) {
         return [];
     }
-    
     // intersect v1-v3 and (v1-v2 or v2-v3)
-    return [[new THREE.Vector3(v1.x, v1.y, v1.z), new THREE.Vector3(v3.x, v3.y, v3.z)]];
+    if (v2.z < z) {
+        return [[intersect_section(v1, v3, z), intersect_section(v2, v3, z)]];
+    }
+    return [[intersect_section(v1, v2, z), intersect_section(v1, v3, z)]];
 }
 
 function roundBlock(n) {
@@ -65,14 +73,39 @@ function add_in_line(a, b) {
         if (a.y >= b.y) {
             a = [b, b = a][0];
         }
-        for (var y = roundBlock(a.y) * blockSize; y < b.y + blockSize; y += blockSize) {
+        for (var y = roundBlock(a.y); y < b.y; y += blockSize) {
             add_cube(roundBlock(a.x + dx * (y - a.y) / dy), y, a.z);
         };
         return;
     }
-    for (var x = roundBlock(a.x); x < b.x + blockSize; x += blockSize) {
+    for (var x = roundBlock(a.x); x < b.x; x += blockSize) {
         add_cube(x, roundBlock(a.y + dy * (x - a.x) / dx), a.z);
     };
+}
+
+function fill_face(v1, v2, v3, z) {
+
+    console.log("!");
+    if (v1.x > v2.x) {
+        v2 = [v1, v1 = v2][0];
+    }
+    if (v1.x > v3.x) {
+        v3 = [v1, v1 = v3][0];
+    }
+    if (v2.x > v3.x) {
+        v2 = [v3, v3 = v2][0];
+    }
+        
+    for (var x = roundBlock(v1.x); x < v3.x; x += blockSize) {
+        let a = new THREE.Vector3(x, v1.y + (x - v1.x) / (v3.x - v1.x) * (v3.y - v1.y), z);
+        if (x <= v2.x) {           
+            let b = new THREE.Vector3(x, v1.y + (x - v1.x) / (v2.x - v1.x) * (v2.y - v1.y), z);
+            add_in_line(a, b);
+        } else {
+            let b = new THREE.Vector3(x, v2.y + (x - v2.x) / (v3.x - v2.x) * (v3.y - v2.y), z);
+            add_in_line(a, b);
+        }
+    }
 }
 
 function initTexture(texture) {
@@ -87,7 +120,7 @@ function initTexture(texture) {
     camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
     camera.position.set(250, 0, -300);
     controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.target = new THREE.Vector3(-15, 70, 0);
+    //controls.target = new THREE.Vector3(-15, 70, 0);
     controls.update();
    
     var cmaterial = new THREE.MeshPhongMaterial({color: 0xffffff, side:THREE.DoubleSide});
@@ -100,26 +133,30 @@ function initTexture(texture) {
         child.geometry.computeFaceNormals();
         child.geometry.computeVertexNormals();
 
-        var geom = new THREE.Geometry();
-        //var geom = new THREE.BoxGeometry(500, 500, 500);
-        //let testMesh = new THREE.Mesh(geom, cmaterial);
+//        var geom = new THREE.Geometry();
+        var geom = new THREE.BoxGeometry(100, 100, 100);
+        let testMesh = new THREE.Mesh(geom, cmaterial);
+//        scene.add(testMesh);
         boxMat = new THREE.MeshPhongMaterial({color: 0xffffff, side:THREE.DoubleSide});
 
-        geom.fromBufferGeometry(child.geometry);
-        console.log(geom);
+//        geom.fromBufferGeometry(child.geometry);
+
         
         slices = 10;
-        var bbox = new THREE.Box3().setFromObject(object);  
-//        var bbox = new THREE.Box3().setFromObject(testMesh);  
+//        var bbox = new THREE.Box3().setFromObject(object);  
+        var bbox = new THREE.Box3().setFromObject(testMesh);  
+        console.log(bbox);
+        
+        
         blockSize = (bbox.max.z - bbox.min.z) / slices;
         boxGeom = new THREE.BoxGeometry(blockSize, blockSize, blockSize);
         boxGeom.computeFaceNormals();
         boxGeom.computeVertexNormals();
         
         for (const face of geom.faces) {
-            let v1 = geom.vertices[face.a].clone().multiplyScalar(900);
-            let v2 = geom.vertices[face.b].clone().multiplyScalar(900);
-            let v3 = geom.vertices[face.c].clone().multiplyScalar(900);
+            let v1 = geom.vertices[face.a].clone();//.multiplyScalar(900);
+            let v2 = geom.vertices[face.b].clone();//.multiplyScalar(900);
+            let v3 = geom.vertices[face.c].clone();//.multiplyScalar(900);
             if (v1.z > v2.z) {
                 v2 = [v1, v1 = v2][0];
             }
@@ -134,23 +171,17 @@ function initTexture(texture) {
                 let z = bbox.min.z + i * blockSize;
                 let face_sects = intersect_face(v1, v2, v3, z, blockSize);
                 
-                for (const sect of face_sects) {
-                    add_in_line(sect[0], sect[1]);
+                if (face_sects.length == 3) {
+                    fill_face(v1, v2, v3, z);
+                } else {
+                    for (const sect of face_sects) {
+                        add_in_line(sect[0], sect[1]);
+                    }
                 }
             }
-        }
-        /*
-        for (const i of Array(slices + 5).keys()) {
-            let z = bbox.min.z + i * blockSize;
             
-            var planeg = new THREE.PlaneGeometry(bbox.max.x - bbox.min.x + 10, bbox.max.y - bbox.min.y + 10);
-            var plane = new THREE.Mesh(planeg, cmaterial);
-            plane.translateX((bbox.max.x + bbox.min.x) / 2 + 5);
-            plane.translateY((bbox.max.y + bbox.min.y) / 2 + 5);
-            plane.translateZ(z);
-            scene.add(plane);
         }
-        */
+        
   
         //scene.add(object);
     });
